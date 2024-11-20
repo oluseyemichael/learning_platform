37a2f61e392e75a489cbf91569c61fae108e03d3
oluseyemichael <oluseyemichael11@gmail.com>
2024-11-20 09:51:51 +0200

    Bug fix

--- learning_platform/core/admin.py
+++ learning_platform/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Course, LearningPath, Module, Quiz, Question, Answer, ModuleProgress, QuizProgress, CourseProgress
+from .models import User, Course, LearningPath, Module, Quiz, Question, Answer, ModuleProgress, QuizProgress, CourseProgress, LearningPathProgress 
 from django.utils.translation import gettext_lazy as _
 
 # User Admin
